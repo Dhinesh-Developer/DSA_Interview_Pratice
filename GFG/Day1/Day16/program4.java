b53c9292135e47b918b76597075fcd14bbf4fae7
DHINESHKUMAR M <dhineshdeveloper07@gmail.com>
2025-11-06 05:46:47 +0530

    Add files via upload

---
+++ GFG/Day1/Day16/program4.java
@@ -0,0 +1,59 @@
+package GFG.Day16;
+/*
+Minimum Number in a sorted rotated array
+Given an array of distinct elements which was initially sorted.
+This array may be rotated at some unknown point.
+ The task is to find the minimum element in the given sorted and rotated array.
+
+Example 1:
+Input:
+N = 10
+arr[] = {2,3,4,5,6,7,8,9,10,1}
+Output: 1
+Explanation: The array is rotated
+once anti-clockwise. So minimum
+element is at last index (n-1)
+which is 1.
+
+Example 2:
+Input:
+N = 5
+arr[] = {3,4,5,1,2}
+Output: 1
+Explanation: The array is rotated
+and the minimum element present is
+at index (n-2) which is 1.
+Your Task:
+The task is to complete the function minNumber() which takes the array arr[] and its starting and ending indices (low and high) as inputs and returns the minimum element in the given sorted and rotated array.
+
+Constraints:
+1 <= N <= 107
+1 <= arr[i] <= 107
+* */
+public class program4 {
+    class Solution {
+        // Function to find the minimum element in sorted and rotated array.
+        static int minNumber(int arr[], int low, int high) {
+            // // Your code here
+            // int minimum = Integer.MAX_VALUE;
+            // for(int i=0;i<arr.length;i++){
+            //     int mini = arr[i];
+            //     minimum = Math.min(mini,minimum);
+            // }
+            // return minimum;
+
+            int min = arr[low];
+            for(int i=low;i<=high;i++){
+                if(arr[i] < min){
+                    min = arr[i];
+                }
+            }
+            return min;
+
+            // Approach 3:
+
+            // Arrays.sort(arr);
+            // return arr[0];
+        }
+    }
+}
