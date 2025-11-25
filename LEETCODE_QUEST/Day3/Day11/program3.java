32ef44d9783ca8d08bcf43ea9e895eb290914658
DHINESHKUMAR M <dhineshdeveloper07@gmail.com>
2025-11-25 07:34:53 +0530

    Add files via upload

---
+++ LEETCODE_QUEST/Day3/Day11/program3.java
@@ -0,0 +1,79 @@
+package LEETCODE_QUEST.Day11;
+/*
+Q3. Ways to Make a Fair Array
+
+You are given an integer array nums.
+ You can choose exactly one index (0-indexed) and remove the element.
+  Notice that the index of the elements may change after the removal.
+
+For example, if nums = [6,1,7,4,1]:
+
+Choosing to remove index 1 results in nums = [6,7,4,1].
+Choosing to remove index 2 results in nums = [6,1,4,1].
+Choosing to remove index 4 results in nums = [6,1,7,4].
+An array is fair if the sum of the odd-indexed values equals the sum of the even-indexed values.
+
+Return the number of indices that you could choose such that after the removal, nums is fair.
+
+
+
+Example 1:
+
+Input: nums = [2,1,6,4]
+Output: 1
+Explanation:
+Remove index 0: [1,6,4] -> Even sum: 1 + 4 = 5. Odd sum: 6. Not fair.
+Remove index 1: [2,6,4] -> Even sum: 2 + 4 = 6. Odd sum: 6. Fair.
+Remove index 2: [2,1,4] -> Even sum: 2 + 4 = 6. Odd sum: 1. Not fair.
+Remove index 3: [2,1,6] -> Even sum: 2 + 6 = 8. Odd sum: 1. Not fair.
+There is 1 index that you can remove to make nums fair.
+Example 2:
+
+Input: nums = [1,1,1]
+Output: 3
+Explanation: You can remove any index and the remaining array is fair.
+Example 3:
+
+Input: nums = [1,2,3]
+Output: 0
+Explanation: You cannot make a fair array after removing any index.
+
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 104
+* */
+public class program3 {
+    class Solution {
+        public int waysToMakeFair(int[] nums) {
+            int n = nums.length;
+            int[] evenPref = new int[n];
+            int[] oddPref = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                if(i > 0) {
+                    evenPref[i] = evenPref[i - 1];
+                    oddPref[i] = oddPref[i - 1];
+                }
+                if(i % 2 == 0) evenPref[i] += nums[i];
+                else oddPref[i] += nums[i];
+            }
+            int totalEven = evenPref[n - 1];
+            int totalOdd = oddPref[n - 1];
+            int count = 0;
+            for(int i = 0;i < n; i++) {
+                int evenLeft = (i == 0) ? 0 : evenPref[i - 1];
+                int oddLeft = (i == 0) ? 0 : oddPref[i - 1];
+                int evenRight = totalOdd - oddPref[i];
+                int oddRight = totalEven - evenPref[i];
+
+                if (evenLeft + evenRight == oddLeft + oddRight) {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+
+}
