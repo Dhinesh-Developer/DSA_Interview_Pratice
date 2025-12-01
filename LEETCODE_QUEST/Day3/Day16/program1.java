f15441ec33edbf511251e3171842f7bb2eb2044d
DHINESHKUMAR M <dhineshdeveloper07@gmail.com>
2025-12-01 05:48:32 +0530

    Add files via upload

---
+++ LEETCODE_QUEST/Day3/Day16/program1.java
@@ -0,0 +1,64 @@
+package LEETCODE_QUEST.Day16;
+
+import java.util.Arrays;
+/*
+
+Code
+
+
+Test Result
+Testcase
+Testcase
+Q2. 3Sum Closest
+Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+Return the sum of the three integers.
+You may assume that each input would have exactly one solution.
+
+Example 1:
+
+Input: nums = [-1,2,1,-4], target = 1
+Output: 2
+Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+Example 2:
+
+Input: nums = [0,0,0], target = 1
+Output: 0
+Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
+
+
+Constraints:
+
+3 <= nums.length <= 500
+-1000 <= nums[i] <= 1000
+-104 <= target <= 104
+* */
+public class program1 {
+    class Solution {
+        public int threeSumClosest(int[] nums, int target) {
+            Arrays.sort(nums);
+            int n = nums.length;
+            int closest = nums[0] + nums[1] + nums[2];
+
+            for (int i = 0;i <n-2; i++) {
+                int left =i + 1;
+                int right = n - 1;
+                while (left < right) {
+                    int sum = nums[i]+ nums[left] + nums[right];
+                    if (Math.abs(sum-target) < Math.abs(closest- target)) {
+                        closest = sum;
+                    }
+
+                    if (sum < target) {
+                        left++;
+                    } else if (sum > target) {
+                        right--;
+                    } else {
+                        return sum;
+                    }
+                }
+            }
+            return closest;
+        }
+    }
+
+}
---
+++ LEETCODE_QUEST/Day3/Day16/program3.java
@@ -0,0 +1,56 @@
+package LEETCODE_QUEST.Day16;
+/*
+Q3. Magical String
+
+A magical string s consists of only '1' and '2' and obeys the following rules:
+
+The string s is magical because concatenating the number of contiguous
+occurrences of characters '1' and '2' generates the string s itself.
+The first few elements of s is s = "1221121221221121122……".
+ If we group the consecutive 1's and 2's in s, it will be "1 22 11 2 1 22 1 22 11 2 11 22 ......"
+ and the occurrences of 1's or 2's in each group are "1 2 2 1 1 2 1 2 2 1 2 2 ......".
+  You can see that the occurrence sequence is s itself.
+
+Given an integer n, return the number of 1's in the first n number in the magical string s.
+
+Example 1:
+
+Input: n = 6
+Output: 3
+Explanation: The first 6 elements of magical string s is "122112" and it contains three 1's, so return 3.
+Example 2:
+
+Input: n = 1
+Output: 1
+* */
+public class program3 {
+    class Solution {
+        public int magicalString(int n) {
+            if (n == 0) return 0;
+            if (n <=3) return 1;
+            int[] s= new int[n + 2];
+            s[0] = 1;
+            s[1]= 2;
+            s[2] =2;
+
+            int head = 2;
+            int tail= 3;
+            int num = 1;
+            int countOnes =1;
+
+            while (tail< n) {
+                int freq =s[head];
+                for (int i= 0; i< freq &&tail < n; i++) {
+                    s[tail] =num;
+                    if (num == 1) countOnes++;
+                    tail++;
+                }
+                num = num== 1 ? 2 : 1;
+                head++;
+            }
+
+            return countOnes;
+        }
+    }
+
+}
