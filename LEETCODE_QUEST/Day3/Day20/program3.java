b388d8ea6f66d53f29d30bbbe4aec9bd9d6cc259
DHINESHKUMAR M <dhineshdeveloper07@gmail.com>
2025-12-05 05:43:10 +0530

    Add files via upload

---
+++ LEETCODE_QUEST/Day3/Day20/program3.java
@@ -0,0 +1,43 @@
+package LEETCODE_QUEST.Day20;
+import java.util.*;
+/*
+Q1. Combinations
+Given two integers n and k, return all possible combinations of k numbers chosen from the range [1, n].
+You may return the answer in any order.
+
+Example 1:
+Input: n = 4, k = 2
+Output: [[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]
+Explanation: There are 4 choose 2 = 6 total combinations.
+Note that combinations are unordered, i.e., [1,2] and [2,1] are considered to be the same combination.
+
+Example 2:
+Input: n = 1, k = 1
+Output: [[1]]
+Explanation: There is 1 choose 1 = 1 total combination.
+ 
+Constraints:
+1 <= n <= 20
+1 <= k <= n
+* */
+public class program3 {
+    class Solution {
+        public List<List<Integer>> combine(int n, int k) {
+            List<List<Integer>> res = new ArrayList<>();
+            f(1,n,k,new ArrayList<>(),res);
+            return res;
+        }
+
+        public void f(int ind,int n,int k,List<Integer> li,List<List<Integer>> res){
+            if(li.size() == k){
+                res.add(new ArrayList<>(li));
+                return;
+            }
+            for(int i=ind;i<=n;i++){
+                li.add(i);
+                f(i+1,n,k,li,res);
+                li.remove(li.size()-1);
+            }
+        }
+    }
+}
