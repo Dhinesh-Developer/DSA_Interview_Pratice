9f06382f4246e722bb3877d31685ba4ed2a3afe9
DHINESHKUMAR M <dhineshdeveloper07@gmail.com>
2025-12-08 05:52:46 +0530

    Add files via upload

---
+++ LEETCODE_QUEST/Day3/Day23/program1.java
@@ -0,0 +1,68 @@
+package LEETCODE_QUEST.Day23;
+import java.util.*;
+/**
+ Q1. Minimum Number of Vertices to Reach All Nodes
+
+ Given a directed acyclic graph, with n vertices numbered from 0 to n-1, and an array edges where edges[i] = [fromi, toi] represents a directed edge from node fromi to node toi.
+
+ Find the smallest set of vertices from which all nodes in the graph are reachable. It's guaranteed that a unique solution exists.
+
+ Notice that you can return the vertices in any order.
+
+
+
+ Example 1:
+
+
+
+ Input: n = 6, edges = [[0,1],[0,2],[2,5],[3,4],[4,2]]
+ Output: [0,3]
+ Explanation: It's not possible to reach all the nodes from a single vertex. From 0 we can reach [0,1,2,5]. From 3 we can reach [3,4,2,5]. So we output [0,3].
+ Example 2:
+
+
+
+ Input: n = 5, edges = [[0,1],[2,1],[3,1],[1,4],[2,4]]
+ Output: [0,2,3]
+ Explanation: Notice that vertices 0, 3 and 2 are not reachable from any other node, so we must include them. Also any of these vertices can reach nodes 1 and 4.
+
+
+ Constraints:
+
+ 2 <= n <= 10^5
+ 1 <= edges.length <= min(10^5, n * (n - 1) / 2)
+ edges[i].length == 2
+ 0 <= fromi, toi < n
+ All pairs (fromi, toi) are distinct.
+
+ Seen this question in a real interview before?
+ 1/5
+ Yes
+ No
+ Accepted
+ 221,729/272.6K
+ Acceptance Rate
+ 81.3%
+*/
+public class program1 {
+    class Solution {
+        public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
+            int[] indegree= new int[n];
+            for (List<Integer> x : edges) {
+                int u = x.get(0);
+                int v = x.get(1);
+                indegree[v]++;
+            }
+
+            List<Integer> result =new ArrayList<>();
+            for (int i = 0;i < n;i++) {
+                if (indegree[i] == 0) {
+                    result.add(i);
+                }
+            }
+
+            return result;
+        }
+    }
+
+}
