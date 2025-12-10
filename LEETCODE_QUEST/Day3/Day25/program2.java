6db8e5fe865d6b1a046691c127ec7da9673adf83
DHINESHKUMAR M <dhineshdeveloper07@gmail.com>
2025-12-10 05:42:54 +0530

    Add files via upload

---
+++ LEETCODE_QUEST/Day3/Day25/program2.java
@@ -0,0 +1,58 @@
+package LEETCODE_QUEST.Day25;
+import java.util.*;
+/*
+Q2. All Paths From Source to Target
+
+Given a directed acyclic graph (DAG) of n nodes labeled from 0 to n - 1,
+ find all possible paths from node 0 to node n - 1 and return them in any order.
+The graph is given as follows: graph[i] is a list of all nodes you can visit from node i
+ (i.e., there is a directed edge from node i to node graph[i][j]).
+
+Example 1:
+
+Input: graph = [[1,2],[3],[3],[]]
+Output: [[0,1,3],[0,2,3]]
+Explanation: There are two paths: 0 -> 1 -> 3 and 0 -> 2 -> 3.
+Example 2:
+
+
+Input: graph = [[4,3,1],[3,2,4],[3],[4],[]]
+Output: [[0,4],[0,3,4],[0,1,3,4],[0,1,2,3,4],[0,1,4]]
+
+
+Constraints:
+
+n == graph.length
+2 <= n <= 15
+0 <= graph[i][j] < n
+graph[i][j] != i (i.e., there will be no self-loops).
+All the elements of graph[i] are unique.
+The input graph is guaranteed to be a DAG.
+* */
+public class program2 {
+    class Solution {
+        public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+            List<List<Integer>> res = new ArrayList<>();
+            List<Integer> path = new ArrayList<>();
+
+            path.add(0); // start sr
+            dfs(0,graph,path,res);
+            return res;
+        }
+
+        public void dfs(int node,int[][] graph,List<Integer> path,List<List<Integer>> res){
+            int n = graph.length;
+
+            if(node == n-1){
+                res.add(new ArrayList<>(path));
+                return;
+            }
+
+            for(Integer x : graph[node]){
+                path.add(x);
+                dfs(x,graph,path,res);
+                path.remove(path.size()-1); // backtrack
+            }
+        }
+    }
+}
