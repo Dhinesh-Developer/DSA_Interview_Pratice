4606a3a72672d00554b151687be8cafe328b8a90
DHINESHKUMAR M <dhineshdeveloper07@gmail.com>
2025-12-31 05:23:08 +0530

    Add files via upload

---
+++ LEETCODE_QUEST/Day3/Day44/program3.java
@@ -0,0 +1,10 @@
+package LEETCODE_QUEST.Day44;
+
+public class program3 {
+    class Solution {
+        public boolean canWinNim(int n) {
+            if(n%4==0) return false;
+            return true;
+        }
+    }
+}
